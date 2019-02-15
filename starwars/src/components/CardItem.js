32c4c10c4ae0c460804bdd552e5e14ed9feffa49
Md Kawsar Hussen <mdkawsarhussen@Mds-MacBook-Pro.local>
2019-02-15 11:36:37 -0500

    Characters details are showing well and css updated

--- starwars/src/components/CardItem.js
+++ starwars/src/components/CardItem.js
@@ -1,19 +1,25 @@
 import React from 'react';
+import './CardItem.css';
 
 function CardItem(props){
     return(
-        <div className = 'card' key={item.name} item= {JSON.stringify(item)}>
-          { props.starwarsChars.map(item=>
-            <h1> {item.name} </h1>
-            <ul className= 'list'>
-                <li> Height : {item.height} </li>
-                <li> Mass : {item.mass} </li>
-                <li> Hair Color : {item.hair_color} </li>
-                <li> Eye Color : {item.eye_color} </li>
-                <li> Birth Year : {item.birth_year} </li>
-                <li> Gender : {item.gender} </li>
-            </ul>
-            )}  
+        <div className = 'characters'>
+          {props.starwarsChars.map(item=>{
+              return(
+                <div className = 'card' key={item.name} item= {JSON.stringify(item)}>
+                    <h1> {item.name} </h1>
+                    <ul className= 'list'>
+                        <li> Height : {item.height} </li>
+                        <li> Mass : {item.mass} </li>
+                        <li> Hair Color : {item.hair_color} </li>
+                        <li> Eye Color : {item.eye_color} </li>
+                        <li> Birth Year : {item.birth_year} </li>
+                        <li> Gender : {item.gender} </li>
+                    </ul>
+                </div>
+               );
+           })}
         </div>
     );
 }
+export default CardItem;
